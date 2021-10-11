cf129d9c24cd54e8792f4f6a409a8aef705bca76
some_guy_oninternet <nonamereal@libero.it>
2021-10-11 16:56:54 +0200

    add tests

---
+++ __tests__/utilities.ts
@@ -0,0 +1,16 @@
+/* eslint-env mocha */
+
+import { decrypt, encrypt } from '../src/utilities';
+import { chance } from 'jest-chance';
+
+describe('utilities', () => {
+  describe('encrypt() and decrypt()', () => {
+    it('should return the input after decrypting', () => {
+      const input = chance.string();
+      const key = chance.string();
+      const encrypted = encrypt(input, key);
+
+      expect(decrypt(encrypted, key)).toBe(input);
+    });
+  });
+});
