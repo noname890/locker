0ec2e39121ff68d21558592709acd1d85575b5ca
some_guy_oninternet <nonamereal@libero.it>
2021-10-12 21:09:33 +0200

    create constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+const constants = {
+  INVALID_MASTERKEY: 'Invalid Master Key or corrupt Lockerfile.',
+  ALREADY_EXISTS: 'Lockerfile already exists.',
+  NO_LOCKERFILE_FOUND:
+    'No Lockerfile found. Try `locker --init` to create a Lockerfile.'
+};
+
+export default constants;
