e3e7ae45570ba8d5fe4fc74efddd8865547aa9d8
some_guy_oninternet <nonamereal@libero.it>
2021-10-12 21:09:25 +0200

    add tests for `compress` and `decompress`

--- __tests__/utilities.ts
+++ __tests__/utilities.ts
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 
-import { decrypt, encrypt } from '../src/utilities';
+import { compress, decompress, decrypt, encrypt } from '../src/utilities';
 import { chance } from 'jest-chance';
 
 describe('utilities.ts', () => {
@@ -11,6 +11,26 @@ describe('utilities.ts', () => {
       const encrypted = encrypt(input, key);
 
       expect(decrypt(encrypted, key)).toBe(input);
+    });
+  });
+
+  describe('compress()', () => {
+    it('should not throw', () => {
+      expect(() => compress(chance.string())).not.toThrow();
+    });
+  });
+  describe('decompress()', () => {
+    it('should not throw', () => {
+      const compressed = compress(chance.string());
+      expect(() => decompress(compressed)).not.toThrow();
+    });
+  });
+  describe('compress() and decompress()', () => {
+    it('should return the original string', () => {
+      const input = chance.string();
+      const compressed = compress(input);
+
+      expect(decompress(compressed)).toBe(input);
     });
   });
 });
